2ec4bf2e3823caee72a2fdccfc0be3f501f5fa11
JohnRey-POGI <102586069+JohnRey-POGI@users.noreply.github.com>
2024-03-17 19:52:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function mouseOver(element) {
     // element.style.background='linear-gradient(to right, red, lightblue)';
     element.innerHTML = 'Hello PANGET'
-    element.style.fontSize = '5rem'
+    element.style.fontSize = '3rem'
 }
 function mouseOut(element) {
     // element.style.background='linear-gradient(to right, blue, lightblue)';
     element.innerHTML = 'Hello WORLD'
-    element.style.fontSize = '3rem'
+    element.style.fontSize = '2rem'
 }
